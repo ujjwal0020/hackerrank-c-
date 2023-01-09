7a018e56d79afc48438d84ec589dc27728821296
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2023-01-09 09:47:19 +0530

    Create BASIC DATA TYPES.CPP

---
+++ BASIC DATA TYPES.CPP
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<setprecision(20)<<d<<"\n"<<setprecision(20)<<e<<"\n";
+    return 0;
+}
