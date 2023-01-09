05da30be9e60228abdccc48d33dd0c7ea979fd2d
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2023-01-09 09:50:19 +0530

    Create ARRAYS INTRODUCTION.CPP

---
+++ ARRAYS INTRODUCTION.CPP
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    int i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
